df94af65b852decfc5c687407e67cc05e8ad916b
giamanpanos <90336670+giamanpanos@users.noreply.github.com>
2025-10-01 21:17:42 +0300

    protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // We created the isProtectedRoute function to put the routes that we want to protect and only displayed tpp authenticated users. Then in the clerkMiddleware we put the auth and request and then if the route is a protected one, we redirect users to the sign in page if not logged in
-const isProtectedRoute = createRouteMatcher(["generate-program", "profile"]);
+const isProtectedRoute = createRouteMatcher(["/generate-program", "/profile"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
